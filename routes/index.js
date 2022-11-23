31d6f85926a9f0363b41fbb3f67f06b88e5cc359
V123in4 <vinconloren@gmail.com>
2022-11-23 21:09:12 +0700

    added profile routes

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const testRoute = require("./test.route");
 const registerRoute = require("./register.router");
 const loginRoute = require("./login.router");
+const profileRoute = require("./profile.router");
 
 router.use("/test", testRoute);
 router.use("/register", registerRoute);
 router.use("/login", loginRoute);
+router.use("/profile", profileRoute);
 
 module.exports = router;
