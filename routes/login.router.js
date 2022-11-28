ece0c386aecd29fcd8c45528ac2faaaf6baeeb0c
sanlokaja <sandyloka790@gmail.com>
2022-11-28 23:22:14 +0700

    docs: add cors

--- routes/login.router.js
+++ routes/login.router.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+router.use(express.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(cors());
+
 const { handleLogin, handleCurrentUser } = require("../controllers/login.controller");
 
 const middlewares = require("../middlewares/auth");
--- routes/register.router.js
+++ routes/register.router.js
@@ -1,6 +1,13 @@
 const express = require("express");
-const { handleRegister } = require("../controllers/register.controller");
 const router = express.Router();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+router.use(express.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(cors());
+
+const { handleRegister } = require("../controllers/register.controller");
 
 router.post("/", handleRegister);
 
